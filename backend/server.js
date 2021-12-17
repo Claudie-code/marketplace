56511d3574c21a9263f075632684d257fcc4d63b
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-12-17 07:42:59 +0100

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,27 @@ routes.use(bodyParser.json());
  
 //cors
 routes.use(cors());
- 
+
+//mongoDB client
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://marketplace:marketplacepassword@cluster-marketplace.sl3f7.mongodb.net/marketplace?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// connect to server
 app.listen(PORT, () => {
   console.log(`Server up and running on http://localhost:${PORT}`);
+});
+
+//connect to Database
+client.connect(err => {
+  if(err) { 
+    throw Error(err);
+  }
+  const collection = client.db("marketplace").collection("products");
+  console.log("Successfull connected to database marketplace"); 
+
+  // perform actions on the collection object
+  client.close();
 });
  
 routes.get("/", (req, res) => {
