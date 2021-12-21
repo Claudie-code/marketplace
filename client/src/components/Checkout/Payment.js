21675d53d3e83e2d3248e524191ebb3c7b10f2a5
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-12-21 12:10:50 +0100

    add shipping

--- client/src/components/Checkout/Payment.js
+++ client/src/components/Checkout/Payment.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { processPayment } from '../../lib/service';
 import { useSelector } from 'react-redux';
+import { selectDeliveryCost } from '../../lib/state/selectors';
 
 function Payment({ isValid }) {
   const { items } = useSelector(state => state.cart);
+  const shippingCost = useSelector(selectDeliveryCost);
+  const shipping = {
+    currency: "usd",
+    quantity: 1,
+    amount: shippingCost * 100,
+    name: "Shipping Cost"
+  };
   const processItem = (item) => ({
     currency: "usd",
     quantity: item.quantity,
@@ -11,7 +19,15 @@ function Payment({ isValid }) {
     name: item.name
   });
   const order = items.map(item => processItem(item));
-  console.log("order",order)
-  return (<button className="btn btn-outline-primary btn-lg mt-3 btn-block" onClick={() => processPayment(order)} disabled={isValid}>Checkout</button>);
+  const orderWithShipping = order.concat(shipping);
+  console.log(orderWithShipping)
+  return (
+    <button 
+      className="btn btn-outline-primary btn-lg mt-3 btn-block" 
+      onClick={() => processPayment(orderWithShipping)} 
+      disabled={isValid}
+    >
+        Checkout
+    </button>);
 }
 export default Payment;
