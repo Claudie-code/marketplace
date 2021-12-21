ecc5521baec61263739d0e8d7098a072588f2704
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-12-21 12:00:31 +0100

    add object to processPayment

--- client/src/components/Checkout/Payment.js
+++ client/src/components/Checkout/Payment.js
@@ -1,8 +1,17 @@
-
 import React from 'react';
 import { processPayment } from '../../lib/service';
+import { useSelector } from 'react-redux';
 
 function Payment({ isValid }) {
-  return (<button className="btn btn-outline-primary btn-lg mt-3 btn-block" onClick={() => processPayment({})} disabled={isValid}>Checkout</button>);
+  const { items } = useSelector(state => state.cart);
+  const processItem = (item) => ({
+    currency: "usd",
+    quantity: item.quantity,
+    amount: item.price * 100,
+    name: item.name
+  });
+  const order = items.map(item => processItem(item));
+  console.log("order",order)
+  return (<button className="btn btn-outline-primary btn-lg mt-3 btn-block" onClick={() => processPayment(order)} disabled={isValid}>Checkout</button>);
 }
 export default Payment;
