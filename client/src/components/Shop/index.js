5e8111916af8acf57e2f831212184e70516fbf6b
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2022-02-01 18:54:16 +0100

    filter model url

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory, useLocation } from 'react-router-dom';
 import { fetchProducts } from '../../lib/state/actions';
-import './shop.css';
+import './shop.scss';
 
 const Shop = () => { 
+    const history = useHistory();
+    const location = useLocation();
+    const [ checkedItems, setCheckedItems ] = useState({});
+    const [ showModel, setShowModel ] = useState(false);
     const dispatch = useDispatch();
     const { slug } = useParams();
     const { items } = useSelector(state => state.products);
     const results = slug ? items.filter(item => item.brand.toLowerCase() === slug.toLowerCase()) : items;
+
+    const handleChange = event => {
+        setCheckedItems({
+          ...checkedItems,
+          [event.target.name]: event.target.checked
+        });
+        const params = new URLSearchParams(event.target.name);
+        history.replace({ pathname: location.pathname, search: params.toString() });
+    };
+
+    const checkboxes = [
+        {
+            name: "yeezy350",
+            label: "Yeezy Boost 350"
+        },
+        {
+            name: "jordanHigh",
+            label: "Air Jordan 1 High"
+        },
+        {
+            name: "dunkLow",
+            label: "Dunk Low"
+        }
+    ];
 
     useEffect(() => {
         dispatch(fetchProducts());
@@ -17,29 +46,24 @@ const Shop = () => {
     return (
         <section className="featured section" id="shop">
             <h2 className="section-title">All Products</h2>
-            <div>
+
+            <div className="bd-grid">
                 <div className="dropdown">
-                    <button class="dropdown__button">Model</button>
-                    <div class="dropdown-content">
-                        <div>
-                            <input type="checkbox" id="yeezy350" name="yeezy350"
-                                    checked />
-                            <label for="yeezy350">Yeezy Boost 350</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="jordanHigh" name="jordanHigh"
-                                    checked />
-                            <label for="jordanHigh">Air Jordan 1 High</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="dunkLow" name="dunkLow"
-                                    checked />
-                            <label for="dunkLow">Dunk Low</label>
-                        </div>
+                    <button class="dropdown__button" onClick={() => setShowModel(!showModel)}>Model</button>
+                    <div class={`dropdown__content ${showModel ? "show__model" : ""}`}>
+                        {checkboxes.map(element => (
+                            <div>
+                                <input type="checkbox" id={element.name} name={element.name}
+                                    checked={checkedItems[element.name]} onChange={handleChange} />
+                                <label for={element.name}>{element.label}</label>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </div>
+            </div>  
+            
             <div className="featured__container shop__container bd-grid">
+
                 {results.map(result => (
                 <article className="sneaker">
                     <img src={result.image} alt={result.name} className="sneaker__img" />
